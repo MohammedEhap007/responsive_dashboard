6d64070123b7d4dc6d1ff63e6fee04235f5528b7
MohammedEhap007 <mohammedehap007@gmail.com>
2025-01-29 01:07:42 +0200

    mobile layout done

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard_app/views/widgets/adaptive_layout_builder.dart';
+import 'package:responsive_dashboard_app/views/widgets/custom_drawer.dart';
 import 'package:responsive_dashboard_app/views/widgets/dashboard_desktop_layout.dart';
+import 'package:responsive_dashboard_app/views/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_dashboard_app/views/widgets/dashboard_tablet_layout.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
 
   @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      appBar: MediaQuery.sizeOf(context).width < 800
+          ? AppBar(
+              elevation: 0,
+              scrolledUnderElevation: 0,
+              backgroundColor: const Color(0XFFFAFAFA),
+              leading: IconButton(
+                onPressed: () => scaffoldKey.currentState!.openDrawer(),
+                icon: const Icon(Icons.menu),
+              ),
+            )
+          : null,
       backgroundColor: const Color(0XFFF7F9FA),
+      drawer:
+          MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
       body: AdaptiveLayoutBulider(
-        mobileLayout: (context) => const SizedBox(),
+        mobileLayout: (context) => const DashboardMobileLayout(),
         tabletLayout: (context) => const DashboardTabletLayout(),
         desktopLayout: (context) => const DashboardDesktopLayout(),
       ),
--- lib/views/widgets/dashboard_tablet_layout.dart
+++ lib/views/widgets/dashboard_tablet_layout.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dashboard_app/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dashboard_app/views/widgets/custom_drawer.dart';
-import 'package:responsive_dashboard_app/views/widgets/income_section.dart';
-import 'package:responsive_dashboard_app/views/widgets/my_cards_and_transaction_history_section.dart';
+import 'package:responsive_dashboard_app/views/widgets/dashboard_mobile_layout.dart';
 
 class DashboardTabletLayout extends StatelessWidget {
   const DashboardTabletLayout({super.key});
@@ -19,20 +17,9 @@ class DashboardTabletLayout extends StatelessWidget {
         ),
         Expanded(
           flex: 3,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                AllExpensesAndQuickInvoiceSection(),
-                SizedBox(
-                  height: 24,
-                ),
-                MyCardsAndTransactionHistorySection(),
-                SizedBox(
-                  height: 24,
-                ),
-                IncomeSection(),
-              ],
-            ),
+          child: Padding(
+            padding: EdgeInsets.only(top: 40.0),
+            child: DashboardMobileLayout(),
           ),
         ),
         SizedBox(
